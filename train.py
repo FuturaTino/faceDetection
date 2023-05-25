68f975e7cce569d660521d4b40ef9defa6b57561
FuturaTino <63669705@qq.com>
2023-05-25 21:20:19 +0800

    训练脚本

--- train.py
+++ train.py
@@ -5,42 +5,51 @@
 import torch 
 from data_setup import create_dataloaders
 from model import Resnet50
-from utils import loss_fn
 from pathlib import Path
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor
 from engine import train
-
-
-#hyperparameters
-loss_fn = loss_fn
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Resnet50(196).to(device)
-optimizer = torch.optim.Adam(model.parameters(),lr=0.1)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=20,gamma=0.1)
-train_dir = Path(r'data\WFLW_annotations\WFLW_annotations\list_98pt_rect_attr_train_test\list_98pt_rect_attr_train.txt')
-test_dir = Path(r'data\WFLW_annotations\WFLW_annotations\list_98pt_rect_attr_train_test\list_98pt_rect_attr_test.txt')
-model_save_path = Path('./models')
-epochs = 1000
-batch_size= 512
+from opt import get_opt
 
 
 
-train_dataloader,test_dataloader = create_dataloaders(train_dir,
-                                                      test_dir,
-                                                        batch_size=batch_size,
-                                                        num_workers=4,
-                                                        transform=ToTensor())
-#创建logs文件夹
-Path('./logs').mkdir(exist_ok=True,parents=True)
-writer = SummaryWriter('./logs')
-train(writer,
-        epochs,
-        model,
-        train_dataloader,
-        test_dataloader,
-        loss_fn,optimizer,
-        device,
-        model_save_path=model_save_path,
-        scheduler=scheduler,
-        checkpoint_path=None)
+
+if __name__ == "__main__":
+  #用argparse来管理超参数
+  args = get_opt()
+
+
+  #hyperparameters
+
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  model = Resnet50(196).to(device)
+  loss_fn = torch.nn.MSELoss(reduction="mean")
+  optimizer = torch.optim.Adam(model.parameters(),lr=0.1)
+  scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=20,gamma=0.1)
+
+  train_dir = args.train_dir
+  test_dir = args.test_dir
+  epochs = args.epochs
+  batch_size = args.batch_size
+  model_save_path = args.model_save_path
+
+
+
+  train_dataloader,test_dataloader = create_dataloaders(train_dir,
+                                                        test_dir,
+                                                          batch_size=batch_size,
+                                                          num_workers=4,
+                                                          transform=ToTensor())
+  #创建logs文件夹
+  Path('./logs').mkdir(exist_ok=True,parents=True)
+  writer = SummaryWriter('./logs')
+  train(writer,
+          epochs,
+          model,
+          train_dataloader,
+          test_dataloader,
+          loss_fn,optimizer,
+          device,
+          model_save_path=model_save_path,
+          scheduler=scheduler,
+          checkpoint_path=None)
