2ca5d9e810b84a27869ae505a832bc3516791ead
FuturaTino <63669705@qq.com>
2023-05-25 21:20:24 +0800

    没咋改

--- utils.py
+++ utils.py
@@ -4,19 +4,28 @@
 """
 
 import torch 
-
- 
-def loss_fn(y_pred:torch.Tensor,y:torch.Tensor)->torch.Tensor:
-    """
-    损失函数，所有预测点与实际点的欧氏距离 平均值
-    """
-    return torch.sqrt(torch.sum((y_pred-y)**2,dim=1)).mean()
-
-
-
+# 引入 评价矩阵accuracy
+from torchmetrics.functional import accuracy
+#引入mseLoss
+mse_loss = torch.nn.MSELoss(reduction="sum") 
 if __name__ == '__main__':
     pass
-    y = torch.tensor([[0,0],[1,0],[2,0]])
-    y_pred = torch.tensor([[0,1],[1,1],[2,4]])
+    y = torch.tensor([[[0,0],
+                       [0,1],
+                       [0,2]],
+
+                       [[1,0],
+                        [1,1],
+                        [1,2]]],dtype=torch.float32)
+    print(y.shape)
+    a = 9+ 9 +9 +16 +16 +16
+    y_pred = torch.tensor([[[0,3],
+                       [0,4],
+                       [0,5]],
+                       [[1,4],
+                       [1,5],
+                       [1,6]]],dtype=torch.float32)
     
     print(loss_fn(y_pred,y))
+    print(f'mse_loss:{mse_loss(y_pred,y)}')
+    print(f'accuracy:{accuracy(y_pred,y)}')
