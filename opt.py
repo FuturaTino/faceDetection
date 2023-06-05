1b3aa1795bbaae8563b734431d866b441d90ee8e
FuturaTino <63669705@qq.com>
2023-06-05 18:27:19 +0800

    add argument scheduler_step

--- opt.py
+++ opt.py
@@ -1,5 +1,5 @@
 import argparse
-
+import numpy as np
 def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr',type=float,default=0.1)
@@ -10,5 +10,6 @@ def get_opt():
     parser.add_argument('--test_dir',type=str,default=r'data\WFLW_annotations\WFLW_annotations\list_98pt_rect_attr_train_test\list_98pt_rect_attr_test.txt')
     parser.add_argument('--model_save_path',type=str,default=r'./models')
     parser.add_argument('--log_dir',type=str,default=r'./logs')
-    parser.add_argument('--k',type=int,default=1)
+    parser.add_argument('--k',type=int,default=1,description='计算acc时的阈值')
+    parser.add_argument('--scheduler_step',type=int,default=10,description='学习率调整的步长')
     return parser.parse_args()
