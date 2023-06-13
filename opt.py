682d16c637f4825d9eed5a4341eece50159ff2c4
FuturaTino <63669705@qq.com>
2023-06-14 01:00:07 +0800

    默认训练15轮

--- opt.py
+++ opt.py
@@ -4,7 +4,7 @@ def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr',type=float,default=0.1)
     parser.add_argument('--batch_size',type=int,default=512)
-    parser.add_argument('--epochs',type=int,default=150)
+    parser.add_argument('--epochs',type=int,default=15)
     parser.add_argument('--checkpoint_path',type=str,default=None)
     parser.add_argument('--train_dir',type=str,default=r'data\WFLW_annotations\WFLW_annotations\list_98pt_rect_attr_train_test\list_98pt_rect_attr_train.txt')
     parser.add_argument('--test_dir',type=str,default=r'data\WFLW_annotations\WFLW_annotations\list_98pt_rect_attr_train_test\list_98pt_rect_attr_test.txt')
